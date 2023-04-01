ca47ba2683eba71cf9b1e024088cf55e2676b01f
tripham <gnoob53@example.com>
2023-04-01 13:27:01 +0700

    product & login pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import { SSRProvider } from 'react-bootstrap';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <Provider store={store}>
+      <SSRProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </SSRProvider>
+    </Provider>
+  )
 }
+
+export default MyApp
